276cffe4a1f5541cdb592fbbcea17cff0c744d81
Lin Lexiao <2623459403@qq.com>
2023-11-16 14:57:23 +0800

    CRLF -> LF

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,9 @@
-script_dir=$(dirname $(readlink -f $0))
-root_dir=$script_dir/..
+root_dir=/mnt/c/ProgramData/Jenkins/.jenkins/workspace/jmail
+echo root_dir=$root_dir
 cd $root_dir || exit 2
 mkdir log
 mvn package > log/maven.log
-if [[ $? == 0 ]]; then
+if [[ $? != 0 ]]; then
   echo "ERROR"
 else
   echo "SUCCESS"
